858c7a82ff23f21270fe7c883bca64844744985a
samir <rewritethis100times@yahoo.com>
2021-01-25 12:48:16 +0530

    added some changes

--- weather/src/components/Tempapp.js
+++ weather/src/components/Tempapp.js
@@ -1,48 +1,63 @@
 import React,{useEffect, useState} from "react";
 import "./css/style.css";
 
+const Weather = ({city}) => {
+    return (
+        <div className="info"> 
+            <h2 className="location">
+                <i className="fas fa-street-view"></i>{city.name}
+            </h2>
+            <h1 className="temp">
+               {city.main.temp}°C
+            </h1>
+            <h3 className="tempmin_max"> Min : {city.main.temp_min}°C | Max : {city.main.temp_max}°C</h3>
+        </div>
+        );
+}
+
 const Tempapp = () => {
 
-    const [city,setcity] =useState(null);
-    const [search, setsearch] = useState("");
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState('');
 
-useEffect( ()=> {
     const fetchapi = async () => {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=0fb48b1af5c39363abd479a9dc7e64e0`;
-        const response = await fetch(url);
-        const resjson = await response.json();
-        setcity(resjson.main);
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${search.toLowerCase()}&units=metric&appid=0fb48b1af5c39363abd479a9dc7e64e0`;
+        fetch(url)
+            .then(result => result.json())
+            .then(data => {
+                if (data.cod === 200)
+                    setCity(data);
+                else
+                    setCity(null);
+            })
+            .catch(err => setCity(null))
     }
-    fetchapi();
-},[search])
+
+    useEffect(() => {
+        fetchapi();
+    }, [search]);
 
     return(
-        <>
-        <div className="box"> 
-            <div className="inputData">
-            <input 
-            type="search"
-            className="inputFeild" 
-            onChange={ (event) =>{setsearch(event.target.value)} } />
+        <div>
+            <div className="box"> 
+                <div className="inputData">
+                    <input 
+                        type="search"
+                        className="inputField" 
+                        onChange={ ({target}) => setSearch(target.value) } />
+
+                </div>
+            
+                {city ? <Weather city={city}/> : <p className="errormsg">No city found</p> }
+            
+                <div className="wave -one"></div>
+                <div className="wave -two"></div>
+                <div className="wave -three"></div>
+
             </div>
-        {!city ? ( <p className="errormsg"> No city found</p>) : (
-        <div className="info"> 
-        <h2 className="location">
-            <i className="fas fa-street-view"></i>{search}
-        </h2>
-        <h1 className="temp">
-           {city.temp}°C
-        </h1>
-        <h3 className="tempmin_max"> Min : {city.temp_min}°C | Max : {city.temp_max}°C</h3>
+
         </div>
-         )}
-        <div className="wave -one"></div>
-        <div className="wave -two"></div>
-        <div className="wave -three"></div>
-        </div>
-       
-        </>
-    )
+    );
 }
 
 export default Tempapp;
